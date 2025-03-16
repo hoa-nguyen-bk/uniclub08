0eaede7a90ee519c4bf6e16d3f596412f8816d97
hoa-nguyen-bk <haveaniceday6174@gmail.com>
2025-03-16 10:08:11 +0700

    buoi 41 ready

--- src/main/java/com/cybersoft/uniclub08/controller/AuthenticationController.java
+++ src/main/java/com/cybersoft/uniclub08/controller/AuthenticationController.java
@@ -4,13 +4,11 @@ import com.cybersoft.uniclub08.payload.response.BaseResponse;
 import com.cybersoft.uniclub08.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("http://127.0.0.1:8000/")
 public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
